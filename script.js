d947eb445bb1541a9d4ce46e27119d253e8f2082
maryandrioli <maryandrioli@gmail.com>
2023-02-10 20:50:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+const palavra_secreta = document.getElementById("palavra_secreta");
+const resultado = document.getElementById("resultado");
+const palavra = document.getElementById("palavra");
+const erros = document.getElementById("erros");
+const teclado = document.getElementById("teclado");
+
+let palavra_secreta_valor = "";
+let palavra_adivinhada = [];
+let erros_count = 0;
+
+palavra_secreta.addEventListener("input", function() {
+  palavra_secreta_valor = palavra_secreta.value.toUpperCase();
+  setTimeout(function() {
+    palavra_secreta.value = "";
+  }, 5000);
+  reiniciar();
+});
+
+function reiniciar() {
+  palavra_adivinhada = [];
+  erros_count = 0;
+  resultado.innerHTML = "";
+  erros.innerHTML = "";
+  teclado.innerHTML = "";
+
+  for (let i = 0; i < palavra_secreta_valor.length; i++) {
+    palavra_adivinhada.push("_");
+  }
+
+  palavra.innerHTML = palavra_adivinhada.join(" ");
+
+  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  for (let i = 0; i < letras.length; i++) {
+    const botao = document.createElement("button");
+    botao.innerHTML = letras[i];
+    botao.addEventListener("click", function() {
+      adivinhar(letras[i]);
+    });
+    teclado.appendChild(botao);
+  }
+}
+
+function adivinhar(letra) {
+  let acerto = false;
+  for (let i = 0; i < palavra_secreta_valor.length; i++) {
+    if (palavra_secreta_valor[i] === letra) {
+      palavra_adivinhada[i] = letra;
+      acerto = true;
+    }
+  }
+
+  if (!acerto) {
+    erros_count++;
+  }
+
+  palavra.innerHTML = palavra_adivinhada.join(" ");
+  erros.innerHTML = "Erros: " + erros_count;
+
+  if (palavra_adivinhada.indexOf("_") === -1) {
+    resultado.innerHTML = "Você ganhou!";
+  } else if (erros_count === 6) {
+    resultado.innerHTML = "Você perdeu!";
+  }
+}
