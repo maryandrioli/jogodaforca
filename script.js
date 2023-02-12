5858b5fa55da3902710bcc79fc68d9023a89eed5
maryandrioli <maryandrioli@gmail.com>
2023-02-12 12:36:44 -0300

    inserção de sons
    atualização do código para ter acesso aos sons de cada letra. Tambem terá uma pasta sounds adicionada e atualizada. Foi modificado o nome do jogo também.

--- script.js
+++ script.js
@@ -9,56 +9,63 @@ let palavra_adivinhada = [];
 let erros_count = 0;
 
 palavra_secreta.addEventListener("input", function() {
-  palavra_secreta_valor = palavra_secreta.value.toUpperCase();
-  setTimeout(function() {
-    palavra_secreta.value = "";
-  }, 5000);
-  reiniciar();
+palavra_secreta_valor = palavra_secreta.value.toUpperCase();
+setTimeout(function() {
+palavra_secreta.value = "";
+}, 5000);
+reiniciar();
 });
 
 function reiniciar() {
-  palavra_adivinhada = [];
-  erros_count = 0;
-  resultado.innerHTML = "";
-  erros.innerHTML = "";
-  teclado.innerHTML = "";
+palavra_adivinhada = [];
+erros_count = 0;
+resultado.innerHTML = "";
+erros.innerHTML = "";
+teclado.innerHTML = "";
 
-  for (let i = 0; i < palavra_secreta_valor.length; i++) {
-    palavra_adivinhada.push("_");
-  }
+for (let i = 0; i < palavra_secreta_valor.length; i++) {
+palavra_adivinhada.push("_");
+}
 
-  palavra.innerHTML = palavra_adivinhada.join(" ");
+palavra.innerHTML = palavra_adivinhada.join(" ");
 
-  const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  for (let i = 0; i < letras.length; i++) {
-    const botao = document.createElement("button");
-    botao.innerHTML = letras[i];
-    botao.addEventListener("click", function() {
-      adivinhar(letras[i]);
-    });
-    teclado.appendChild(botao);
-  }
+const letras = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+for (let i = 0; i < letras.length; i++) {
+const botao = document.createElement("button");
+botao.innerHTML = letras[i];
+botao.addEventListener("click", function() {
+adivinhar(letras[i]);
+});
+botao.addEventListener("mouseover", function() {
+const audio = new Audio("sounds/" + letras[i].toLowerCase() + ".mp3");
+audio.play();
+});
+teclado.appendChild(botao);
+}
 }
 
 function adivinhar(letra) {
-  let acerto = false;
-  for (let i = 0; i < palavra_secreta_valor.length; i++) {
-    if (palavra_secreta_valor[i] === letra) {
-      palavra_adivinhada[i] = letra;
-      acerto = true;
-    }
-  }
-
-  if (!acerto) {
-    erros_count++;
-  }
-
-  palavra.innerHTML = palavra_adivinhada.join(" ");
-  erros.innerHTML = "Erros: " + erros_count;
-
-  if (palavra_adivinhada.indexOf("_") === -1) {
-    resultado.innerHTML = "Você ganhou!";
-  } else if (erros_count === 6) {
-    resultado.innerHTML = "Você perdeu!";
-  }
+let acerto = false;
+for (let i = 0; i < palavra_secreta_valor.length; i++) {
+if (palavra_secreta_valor[i] === letra) {
+palavra_adivinhada[i] = letra;
+acerto = true;
 }
+}
+
+if (!acerto) {
+erros_count++;
+}
+
+palavra.innerHTML = palavra_adivinhada.join(" ");
+erros.innerHTML = "Erros: " + erros_count;
+
+if (palavra_adivinhada.indexOf("_") === -1) {
+resultado.innerHTML = "Você ganhou!";
+} else if (erros_count === 6) {
+resultado.innerHTML = "Você perdeu!";
+}
+}
+
+
+
